33355a212e6cc4cb4e0f5ebe7c2bcdaaff2556fb
Pete Gadomski <pete.gadomski@gmail.com>
2026-02-04 07:16:10 -0700

    refactor: about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,18 @@
-import { Text, VStack } from "@chakra-ui/react";
+import { Stack, Text } from "@chakra-ui/react";
 import Section from "./Section";
 import TextLink from "./TextLink";
 
 function About() {
   return (
     <Section id="about" title="About">
-      <VStack gap={6} align="stretch">
-        <Text fontSize="lg" color="gray.700" lineHeight="tall">
+      <Stack
+        gap={6}
+        align="stretch"
+        fontSize={"lg"}
+        color="gray.700"
+        lineHeight={"tall"}
+      >
+        <Text>
           The <strong>stac-geoparquet</strong> specification describes how to
           encode <TextLink href="https://stacspec.org">STAC</TextLink> items in
           the{" "}
@@ -16,18 +22,20 @@ function About() {
           provides a standardized way to store many STAC items as columnar data,
           enabling efficient queries and analysis at scale.
         </Text>
-        <Text fontSize="lg" color="gray.700" lineHeight="tall">
+
+        <Text>
           <strong>stac-geoparquet</strong> complements existing STAC APIs and
           static catalogs, and is be extremely performant for bulk data access
           and analytics workloads.
         </Text>
-        <Text fontSize="lg" color="gray.700" lineHeight="tall">
+
+        <Text>
           Read the{" "}
           <TextLink href="https://radiantearth.github.io/stac-geoparquet-spec/latest/">
             latest version of the specification.
           </TextLink>
         </Text>
-      </VStack>
+      </Stack>
     </Section>
   );
 }
